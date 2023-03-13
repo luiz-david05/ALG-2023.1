82bdefacd9765414adde5119e43188b7f69f05ec
Luiz David Silva Gomes <96085848+luiz-david05@users.noreply.github.com>
2023-03-13 18:19:42 -0300

    M02: Atividade C | GPT* --> Funções

---
+++ calculadora_agua.py
@@ -0,0 +1,32 @@
+from auxiliar_functions import *
+
+
+def main():
+    peso = get_int_number("peso: ")
+
+    qtd_agua_moderada = calcular_qtd_agua(peso, 35)
+
+    # convertendo a quantidade de ml em litros
+    qtd_litros_agua = converter_ml_para_litro(qtd_agua_moderada)
+
+    print(
+        f"\nquantidade recomendada de água atividade moderada: {qtd_litros_agua:.1f} litro(s) ")
+
+    qtd_agua_intensa = calcular_qtd_agua(peso, 45)
+
+    qtd_litros_agua = converter_ml_para_litro(qtd_agua_intensa)
+
+    print(
+        f"\nquantidade recomendada de água atividade intensa: {qtd_litros_agua:.1f} litro(s) ")
+
+
+def calcular_qtd_agua(peso, ml):
+    # arredondando o peso para ignorar a parte fracionaria do peso (se tiver)
+    return ml * round(peso)
+
+
+def converter_ml_para_litro(n):
+    return n / 1000
+
+
+main()
---
+++ tempo_corrida.py
@@ -0,0 +1,30 @@
+from auxiliar_functions import *
+
+
+def main():
+    distancia = get_float_number("metros: ")
+    velocidade = get_float_number("velocidade(km/h): ")
+
+    velocidade_metros_por_minuto = converter_velocidade_para_metros_por_minutos(
+        velocidade)
+
+    tempo = calcular_tempo_prova(distancia, velocidade_metros_por_minuto)
+
+    tempo_prova_minutos = converter_para_minutos(tempo)
+
+    print(f"\ntempo de prova: {tempo_prova_minutos:.1f} minuto(s)")
+
+
+def converter_velocidade_para_metros_por_minutos(velocidade):
+    return (velocidade * 1000) / 60
+
+
+def calcular_tempo_prova(distancia, velocidade):
+    return (distancia / (velocidade * 1000)) * 60
+
+
+def converter_para_minutos(tempo):
+    return tempo * 60
+
+
+main()
